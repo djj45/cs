3a0a3fd2f20f89e1b1a2bd3fd48c84e49a458c31
djj45 <ssjj_djj45@163.com>
2022-05-17 23:26:21 +0800

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Formatting = Newtonsoft.Json.Formatting;
+using System.Collections;
+using System.Net;
+using System.Data;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (StreamWriter srt = File.CreateText("1.txt"))
+            {
+                srt.WriteLine("111");
+                srt.WriteLine("111");
+            };
+
+        }
+    }
+}
+
+
